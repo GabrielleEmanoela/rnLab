c760bfa761583db9b94ace149ab632ee4843a8d1
Gabrielle Silva <biela.a09@gmail.com>
2021-03-21 17:15:56 -0300

    Sistema de módulo do Ecmasript primeira maneira.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 
+import X, { Comp1, Comp2 } from './components/Multi'
 import Primeiro from './components/Primeiro_comp'
 
-export default () => <Primeiro>
-</Primeiro>
+export default () =>
+
+    <View>
+        <Primeiro></Primeiro>
+        <X> </X>
+        <Comp1></Comp1>
+        <Comp2></Comp2>
+    </View>
+
---
+++ src/components/Multi.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Comp() {
+    return <Text>
+        Com #Oficial
+    </Text>
+}
+export function Comp1() {
+    return <Text>
+        Comp #01
+    </Text>
+}
+export function Comp2() {
+    return <Text>
+        Comp #02
+    </Text>
+}
+
