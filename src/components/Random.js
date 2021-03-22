b94a2e9430236cc72e3f5b4730c907de2513090d
Gabrielle Silva <biela.a09@gmail.com>
2021-03-21 21:05:14 -0300

    Correção Do Desafio.

--- src/components/Random.js
+++ src/components/Random.js
@@ -4,9 +4,7 @@ import Style from './Sytle';
 
 export default (number) => {
   console.warn(number);
-  return (
-    <Text style={Style.textStyle}>
-      Select number {Math.random((number.min += number.max))}
-    </Text>
-  );
+  const verification = number.max - number.min + 1;
+  const total = parseInt(Math.random() * verification) + number.min;
+  return <Text style={Style.textStyle}>Select number {total}</Text>;
 };
